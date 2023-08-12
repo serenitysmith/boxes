21f939637039c2bee28e6c30fdace3b03cc07a54
serenitysmith <serenitysmith24@gmail.com>
2023-08-11 20:21:59 -0400

    react state boxes challenge 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+
+
+import React, {useState} from 'react';
+// import it and initialize your state with these values. Let's say the array is called boxData:
+//javascript
+import Boxes from './boxes'
 import './App.css';
 
+
+// Now, you can use the map function to iterate through your state array and render empty squares for each entry. You can use HTML/CSS or JSX to create these squares. For example:
+
+
+
+/**
+     * Challenge part 1:
+     * 1. Initialize state with the default value of the
+     *    array pulled in from boxes.js
+     * 2. Map over that state array and display each one
+     *    as an empty square (black border, transparent bg color)
+     *    (Don't worry about using the "on" property yet)
+     */
+
+
+
+// In this code, we're mapping over the state array and rendering a <div> for each element. The key attribute is set to the index to uniquely identify each square. The className is set to "empty-square" to apply any relevant styling.
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  // setting up a state varible 
+
+  const [square, setState] = useState(Boxes);
+
+  // using paraentheses instea of curly brackets uses implicit return 
+
+  // Warning: Each child in a list should have a unique "key" prop. Check the render method of `App`. See https://reactjs.org/link/warning-keys for more information. at div at App (exe1.bundle.js:29:48)
+//index.html
+
+
+
+// mapped over the square array thats in the boxes js file 
+// 
+const squareElements = square.map(square => (
+<div className='box' key={square.id}></div>
+))
+
+
+// returning a JSX element to be rendered to the DOm 
+   return (
+
+    <main>
+      {squareElements}
+    </main>
+  //   <main className='app'>
+  //     {state.map((box, index) => ( 
+  //       <div
+  //       key={index}
+  //       className='empty-square'>
+
+  //       </div>
+
+  //     ))}
+  //   </main>
+  //  )
+
+)}
 
 export default App;
+
+
+/// explanation of above code 
+
+
+
+
+// The import React, { useState } from 'react'; line imports the necessary components from the 'react' library, including the useState hook for managing state.
+// import Boxes from './boxes'; imports a module named 'Boxes' from a file named 'boxes.js' (assuming it's in the same directory). This module seems to provide an array of data.
+// Functional Component 'App':
+
+// function App() defines the main component named 'App'.
+// State Management:
+
+// const [square, setState] = useState(Boxes); initializes a state variable named 'square' using the useState hook. The initial state is set to the array provided by the 'Boxes' module.
+// Mapping Over 'square' Array:
+
+// const squareElements = square.map((square) => (...)); maps over the 'square' array using the map function. For each item in the array, it generates a JSX element inside the parentheses.
+// JSX Rendering:
+
+// <div className='box' key={square.id}></div> represents an empty <div> element with a CSS class of 'box'. The key attribute is set to the id property of the 'square' object. This is crucial for efficient rendering and updates.
+// Returning JSX:
+
+// The return statement within the App component returns JSX elements to be rendered in the application. It renders the array of 'box' elements generated using the map function.
+// Export:
+
+// export default App; exports the 'App' component as the default export of this module, making it available to other parts of the application.
+// This code seems to be a basic setup for rendering a list of empty boxes (div elements) using React components and state management. The actual rendering of these boxes will require additional styling from the associated CSS and more detailed data provided by the 'Boxes' module.
---
+++ src/boxes.js
@@ -0,0 +1,31 @@
+// export of aray off objects 
+
+
+
+
+export default [
+    {
+        id: 1,
+        on: true
+    },   
+    {
+        id: 2,
+        on: false
+    },   
+    {
+        id: 3,
+        on: true
+    },   
+    {
+        id: 4,
+        on: true
+    },   
+    {
+        id: 5,
+        on: false
+    },   
+    {
+        id: 6,
+        on: false
+    },   
+]
